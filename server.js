2b3b8b26554bce387421a07dfe942b89677a4942
Leif Reinert <leif.reinert@gmail.com>
2020-06-18 14:33:13 -0400

    Update to fix security vulnerabilities

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 //using express nodejs web framework for now
 var app = require('express')();
-var http = require('http').Server(app);
+var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000
 
 //Server connection
 
 //serving out on to web page
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
         res.sendFile(__dirname + '/index.html');
 });
 
 //actions to be performed on socket io connection
-io.on('connection', function(socket) {
+io.on('connection', (socket) => {
         console.log('a user connected');
 
-        socket.on('io message', function(msg) {
+        socket.on('io message', (msg) => {
                 io.emit('io message', msg);
                 console.log('message: ' + msg);
         });
@@ -27,7 +27,7 @@ io.on('connection', function(socket) {
 
 
 // listen on localhost, port 3000
-http.listen(port, function() {
+http.listen(port, () => {
         console.log('listening on *:' + port);
 });
 
